f250ee5ccb7afe9cab8f79906a3f1f7ce9b1a3ba
Dalia <dalia.sawaya@flatironschool.com>
2020-05-26 17:00:55 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 function createStore(candyReducer){
   let state
 
-
   function dispatch(action) {
+    console.log("dispatch called")
     state = candyReducer(state, action)
+    console.log(state)
+    render()
   }
 
   function getState(){
@@ -34,6 +36,11 @@ function render() {
     throw new Error("the store's state has not been defined yet")
   }
 };
+
+
+let store = createStore(candyReducer)
+store.dispatch({ type: '@@INIT' })
+store.dispatch({type: 'ADD_CANDY', candy: "Jelly Beans"})
 
 // use your createStore function and the functions provided here to create a store
 // once the store is created, call an initial dispatch
