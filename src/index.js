adca90e6ddde86d6f2da99a740f4e1ae8991abdc
Maria Le <hp.mars.le@gmail.com>
2020-06-05 14:49:23 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -34,7 +34,8 @@ function render() {
   }
 };
 
-let store = createStore(candyReducer)
-store.dispatch({type: "@@INIT"})
 // use your createStore function and the functions provided here to create a store
 // once the store is created, call an initial dispatch
+let store = createStore(candyReducer)
+store.dispatch({type: "@@INIT"})
+store.dispatch({ type: "ADD_CANDY", candy: "sourpatch" })
